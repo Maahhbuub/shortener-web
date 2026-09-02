0aa650f496052495be50ee020bbb408073f600f9
Mahbub <mrahman221441@gmail.com>
2026-09-03 01:54:28 +0600

    feat(home): add render wake up api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styles from './Home.module.css';
 
 import Hero from '../components/Hero';
 
+import api from '../api/api';
+
 function Home() {
+
+    // trigger the render server
+    async function pingHealth() {
+        const res = await api.get('/health');
+        console.log(res?.data);
+    }
+    useEffect(() => {
+        pingHealth();
+    }, [])
+
     return (
         <div className={`${styles.home}`}>
             <Hero />
