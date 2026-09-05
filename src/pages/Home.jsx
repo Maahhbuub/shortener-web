017b146ed80cb5cb5ce4022f73b4874ed1048799
Mahbub <mrahman221441@gmail.com>
2026-09-05 15:01:50 +0600

    fix(home): remove uptime request

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,7 @@ import Hero from '../components/Hero';
 import api from '../api/api';
 
 function Home() {
-
-    // trigger the render server
-    async function pingHealth() {
-        const res = await api.get('/health');
-        console.log(res?.data);
-    }
-    useEffect(() => {
-        pingHealth();
-    }, [])
-
+    
     return (
         <div className={`${styles.home}`}>
             <Hero />
